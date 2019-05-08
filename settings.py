f159bf2818aa39ff34e72724c2898794a331cbbf
PCSchmitz <46964457+PCSchmitz@users.noreply.github.com>
2019-05-08 11:25:50 +0100

    Test bestand

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
 
 # DATABASE_URI = "postgresql://perenna:perenna@localhost/perenna"
-DATABASE_URI = "mssql+pyodbc://PaulS:Perenna01@localhost:1433/PbPrototype?driver=ODBC+Driver+17+for+SQL+Server"
+DATABASE_URI = "mssql+pyodbc://PaulS:Perenna01@DESKTOP-V2HKR22\SQLEXPRESS/PbPrototype?driver=ODBC+Driver+17+for+SQL+Server"
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
